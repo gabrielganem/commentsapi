46e226c63aba27c18cf310d291c10f4977349df8
Joao Gabriel Ganem Barbosa <jgabriel@MBPro-Ganem.local>
2022-10-05 22:06:18 -0300

    Base laravel

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,18 +16,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('comments')->insert([   
-        [
-            'name' => 'John Smith',
-             'message' => 'Hi Jane, please read this post.',
-             'parent_id' => null,
-        ],
-        [
-            'name' => 'The Hound',
-             'message' => "Im gonna have to eat every chicken in this room!",
-             'parent_id' => null,
-        ],
-    ]);
 
         
     }
--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/comments', [CommentController::class, 'index'])->name('index');
-Route::post('/comments', [CommentController::class, 'store']);
-Route::post('/comments/{id}/reply', [CommentController::class, 'reply']);
